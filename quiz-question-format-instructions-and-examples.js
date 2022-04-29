504d8a84346b6fc468e4a13c89b985f6147d5fae
Martyn-Conkling <23250543+Martyn-Conkling@users.noreply.github.com>
2022-04-28 18:44:48 -0700

    Made some updates to the quiz

--- quiz-question-format-instructions-and-examples.js
+++ quiz-question-format-instructions-and-examples.js
@@ -1,14 +1,12 @@
 function arraysQuizExample(){
-
-
-    function arraysQuiz1Question1(){
+    function arraysQuizQuestion1(){
         // Write the prompt for the question
         // Have them complete some code which will result in console.logging something
 
         // either have a return statement or a console.log for the student to be able to have the answer returned to the console
     }
 
-    function arraysQuiz1Question2(){
+    function arraysQuizQuestion2(){
 
 
 
@@ -21,15 +19,41 @@ function arraysQuizExample(){
         // return (x !== y);
          return (x == y)
      
-	
 
     };
 
+    
+let questionsArray = [arraysQuizQuestion1(), arraysQuizQuestion2()];
+console.log(questionsArray[1]);
 
-    console.log("The Answer to Question 1 is: ", arraysQuiz1Question1());
-    console.log("The Answer to Question 2 is: ", arraysQuiz1Question2());
+    // console.log("The Answer to arraysQuizQuestion1 is: ", );
+    // console.log("The Answer to arraysQuizQuestion2 is: ", );
     // arraysQuiz1Question1();
     // arraysQuiz1Question2();
+
+}
+
+//Make some sort of class to organize sub questions?
+//Maybe just mainly inherit some core functionality we want to have in each of these question functions
+
+// how do we give questions to this Quiz section object?
+// We want this Quiz Section object to be able to store the questions (basically the name of the function)
+// We want to be able to programatically console log the results from the questions
+class QuizSection{
+  constructor(quizTopic, numberOfQuestions){
+    this.quizTopic
+  }
+
+  logAnswer(){
+    
+  }
+
+  //quiz topic property
+  //method that will console log the asnwer to a question
+
+}
+
+class QuizQuestion{
 
 }
 
